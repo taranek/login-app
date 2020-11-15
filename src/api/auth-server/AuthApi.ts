439ec1fbbd3c96b485b7f14573ec25235d0b0b47
taranek <t.taranek27@gmail.com>
2020-11-15 13:39:56 +0100

    feature(): added mock api models

---
+++ src/api/auth-server/AuthApi.ts
@@ -0,0 +1,22 @@
+import { AuthRequest } from "./model";
+import axios from 'axios';
+
+export const loginUser = async (request: AuthRequest) => {
+ /*   const axiosResponse = await axios.post("http://localhost:3001/api/auth/login", request, {headers:{
+        Accept: 'application/json',
+        'Content-Type':'application/json',
+        },withCredentials: true})
+    console.log(axiosResponse)*/
+    const response = await fetch(new Request("http://localhost:3001/api/auth/login", {
+        method: "POST",
+        headers: {
+            "Accept":'application/json',
+            "Content-Type":'application/json',
+            "Access-Control-Allow-Credentials": "true",
+            "Cache":'no-cache'
+        },
+        mode: "cors",
+        credentials: 'include',
+        body: JSON.stringify(request)
+    }))
+}
